0757043a388aeb2edcb2ed3b91996a482c782ea3
Leshan21 <leshanpasindu12@gmail.com>
2024-06-11 14:47:28 +0530

    use Math method

--- CODE/Lecture 1/index.js
+++ CODE/Lecture 1/index.js
@@ -1,1 +1,13 @@
-console.log("test one!!!");
+let a;
+let b;
+let c;
+
+/*a = window.prompt("Enter side A");
+a = Number(a);
+
+b = window.prompt("Enter side B");
+b = Number(b);
+
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+alert(c);*/
