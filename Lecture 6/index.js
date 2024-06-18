6b61758f03929d670b1f6ac1992e6102ee05b656
Leshan21 <leshanpasindu12@gmail.com>
2024-06-18 22:37:40 +0530

    Template-literals

---
+++ Lecture 6/index.js
@@ -0,0 +1,7 @@
+let userName = "Leshan";
+let items = 3;
+let total = 75;
+
+console.log(`hello ${userName}`);
+console.log(`you have ${items} items in your cart`);
+console.log(`your totel is $${total}`);
